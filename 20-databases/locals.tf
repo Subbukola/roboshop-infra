da1ce16681b21dcf03b67d8032c57e350a07dcd9
subbu <subbu@example.com>
2026-03-20 23:38:35 +0530

    updated

--- 20-databases/locals.tf
+++ 20-databases/locals.tf
@@ -1,6 +1,6 @@
 locals {
   database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-    database_sg_id = data.aws_ssm_parameter.database_sg_id.value
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     common_tags={
     project=var.project
     environment= var.environment
