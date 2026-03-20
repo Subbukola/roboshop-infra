74de2cb1f2a53bcff27346f1961c5724824aace7
subbu <subbu@example.com>
2026-03-20 23:42:31 +0530

    updated

--- 20-databases/main.tf
+++ 20-databases/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.joindevops.id
   instance_type = "t3.micro"
-  vpc_security_group_ids=[local.database_sg_id]
+  vpc_security_group_ids=[local.mongodb_sg_id]
   subnet_id = local.database_subnet_id
   #iam_instance_profile = aws_iam_instance_profile.bastion.name
 
