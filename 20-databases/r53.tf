2695d89856c6e8ffcc85c9b81edacf365aef4ec9
subbu <subbu@example.com>
2026-03-22 23:14:41 +0530

    commit

--- 20-databases/r53.tf
+++ 20-databases/r53.tf
@@ -1,7 +1,7 @@
 #mongodb
 resource "aws_route53_record" "mongodb" {
-  zone_id = var.zone_id
-  name    = "mongodb-${var.environment}-${var.domain_name}"
+  zone_id = local.domain_zone_id
+  name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
@@ -13,7 +13,7 @@ resource "aws_route53_record" "mongodb" {
 #redis
 resource "aws_route53_record" "redis" {
   zone_id = var.zone_id
-  name    = "redis-${var.environment}-${var.domain_name}"
+  name    = "redis-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.redis.private_ip]
@@ -24,7 +24,7 @@ resource "aws_route53_record" "redis" {
 #mysql
 resource "aws_route53_record" "mysql" {
   zone_id = var.zone_id
-  name    = "mysql-${var.environment}-${var.domain_name}"
+  name    = "mysql-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.mysql.private_ip]
@@ -35,7 +35,7 @@ resource "aws_route53_record" "mysql" {
 #rabbitmq
 resource "aws_route53_record" "rabbitmq" {
   zone_id = var.zone_id
-  name    = "rabbitmq-${var.environment}-${var.domain_name}"
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.rabbitmq.private_ip]
