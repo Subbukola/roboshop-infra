67ede010f129ad76c2add086180a419a6748ddb3
subbu <subbu@example.com>
2026-03-25 00:37:57 +0530

    updated

--- 20-databases/bootstap.sh
+++ 20-databases/bootstap.sh
@@ -3,7 +3,7 @@ component=$1
 environment=$2
 dnf install ansible -y
 cd /home/ec2-user
-git clone https://github.com/Subbukola/ansible_roles.git
-cd ansible_roles
+git clone https://github.com/Subbukola/terraform_roboshop_ansible_roles.git
+cd terraform_roboshop_ansible_roles
 git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
